f98b313df1d9edd364ec819ad0386335617b2e0b
zebluenote <zebluenote@alexcyr.com>
2020-04-05 15:06:05 +0200

    install apache-pack

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+
 /**
  *
  */
 class HomeController
 {
+
+  /**
+   * @var Environment
+   */
+  private $twig;
 
   function __construct($twig)
   {
@@ -16,7 +24,7 @@ class HomeController
 
   public function index(): Response {
 
-    return new Response('Salut les gens');
+    return new Response($this->twig->render('pages/home.html.twig'));
 
   }
 
